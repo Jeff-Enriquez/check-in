1978bcb172891bdebc1ead5e0e836809be9bd402
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-06-02 23:47:05 -0700

    Install dependancies and setup project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { ThemeProvider } from 'react-jss'
+import jss from 'jss'
+import preset from 'jss-preset-default'
+import { SheetsRegistry } from 'react-jss'
+import { JssProvider } from 'react-jss'
+import { BrowserRouter } from 'react-router-dom'
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: [
+      'Work Sans:400,500,700', 'sans-serif',
+      'Space Mono:400,500', 'monospace',
+      'Source Code Pro:400',
+    ]
+  }
+})
+
+const setupJss = () => {
+  jss.setup(preset())
+
+  const sheetsRegistry = new SheetsRegistry()
+
+  const globalStyleSheet = jss.createStyleSheet(
+    {'@global': { 
+        body: { 
+          margin: '0',
+        },
+    }}
+  ).attach()
+
+  sheetsRegistry.add(globalStyleSheet)
+
+  return sheetsRegistry
+}
+
+const sheets = setupJss()
+
+const theme = {
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <JssProvider registry={sheets}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </JssProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
