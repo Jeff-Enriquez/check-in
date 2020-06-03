75b2999cb16e0756a5f6195a067ba509cbb3c1c6
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-06-03 14:21:12 -0700

    Add check-in page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
+import CheckIn from './pages/CheckIn'
 
 function App() {
   return (
     <>
       <Header/>
-      {/* <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/about' component={About}/>
-        <Route exact path='/adding-projects-to-the-index' component={HowToUse}/>
-        <Route exact path='/searching-the-index' component={Search}/>
+      <Switch>
+        <Route exact path='/check-in' component={CheckIn}/>
       </Switch>
-      <Footer/> */}
     </>
   )
 }
---
+++ src/pages/CheckIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from './styles.js'
+
+const Header = () => {
+  const classes = useStyles()
+
+  return (
+    <h1>Check In</h1>
+  )
+}
+
+export default Header
