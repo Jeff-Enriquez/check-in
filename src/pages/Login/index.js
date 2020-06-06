eb06bb01d075e78950637f047988f142a824808e
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-06-05 17:36:59 -0700

    Add form to login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './styles.js'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   const classes = useStyles()
+
+  const handleForm = async e => {
+    e.preventDefault()
+  }
 
   return (
     <div className={classes.page}>
-      <h1 className={classes.pageTitle}>Login</h1>
-      <div className={classes.container}>
-        <h1>Login Page</h1>
-      </div>
+      {/* <form className={classes.inputContainer} onSubmit={handleForm}>
+        <input type="username" className={classes.input} placeholder="Username" autoComplete='username' value={username} onChange={e => setUsername(e.target.value)} />
+        <input type="password" className={classes.input} placeholder="Password" autoComplete='current-password' value={password} onChange={e => setPassword(e.target.value)} />
+      </form> */}
+      	<form className={classes.formContainer}>
+          <h1 className={classes.heading}>Fast Queue</h1>
+					<div className={classes.inputContainer}>
+						<p className={classes.inputHeading}>Username</p>
+            <input type="username" className={classes.input} placeholder="Username" autoComplete='username' value={username} onChange={e => setUsername(e.target.value)} />
+					</div>
+          <div className={classes.inputContainer}>
+						<p className={classes.inputHeading}>Password</p>
+            <input type="password" className={classes.input} placeholder="Password" autoComplete='current-password' value={password} onChange={e => setPassword(e.target.value)} />
+					</div>
+          <button className={classes.loginBtn}>Login</button>
+				</form>
     </div>
   )
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,10 +2,21 @@ import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles( theme => ({
   page: {
+    position: 'relative',
+    boxSizing: 'border-box',
+    width: '100%',
+    height: '100vh',
     padding: '20px',
+    fontFamily: theme.primaryFontFamily,
+    color: theme.primaryTextColor,
+    border: '1px solid red',
+    '& *': {
+      boxSizing: 'border-box',
+    },
   },
-  pageTitle: {
-    margin: '0',
+  heading: {
+    textAlign: 'center',
+    margin: '0 0 20px 0',
     textTransform: 'uppercase',
     letterSpacing: '.2em',
     color: theme.primaryTextColor,
@@ -13,11 +24,38 @@ const useStyles = createUseStyles( theme => ({
     fontSize: '2em',
     fontWeight: 'normal',
   },
-  container: {
-    fontFamily: 'lato, sans-serif',
-    maxWidth: '485px',
-    margin: '50px auto 0 auto',
-  },   
+  formContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '28px 38px',
+    margin: '0 auto',
+    width: '345px',
+    boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.2), 0px 3px 14px rgba(0, 0, 0, 0.12), 0px 8px 10px rgba(0, 0, 0, 0.14)',
+    borderRadius: '12px',
+    fontFamily: theme.secondaryFontFamily,
+  },
+  inputHeading: {
+    fontSize: '.8em',
+    margin: '0 0 0 2px',
+  },
+  input: {
+    width: '100%',
+  },
+  // inputContainer: {
+  //   margin: '40px auto 0 auto',
+  //   width: 'fit-content',
+  //   '& input:first-child': {
+  //     margin: '20px 0',
+  //   },
+  //   '& input': {
+  //     boxSizing: 'border-box',
+  //     display: 'block',
+  //     width: '200px',
+  //     padding: '5px',
+  //   },
+  // },
 }))
 
 export default useStyles
